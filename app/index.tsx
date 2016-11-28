900fcf7c5ffe0f485b00f14079c82263b2f1c18d
Stereokai <tom@stereokai.com>
2016-11-28 15:17:22 +0200

    Add mobx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,44 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
 
-import { Hello } from "./components/Hello";
+class AppState {
+  @observable timer = 0;
+
+  constructor() {
+      setInterval(() => {
+          this.timer += 1;
+      }, 1000);
+  }
+
+  resetTimer() {
+      this.timer = 0;
+  }
+}
+
+@observer
+class TimerView extends React.Component<{appState: AppState}, {}> {
+  render() {
+    return (
+      <div>
+        <button onClick={this.onReset}>
+          Seconds passed: {this.props.appState.timer}
+        </button>
+        <DevTools />
+      </div>
+    );
+  }
+
+  onReset = () => {
+      this.props.appState.resetTimer();
+  }
+};
+
+const appState =  new AppState();
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+  <TimerView appState={appState} />,
+  document.getElementById('root')
 );
